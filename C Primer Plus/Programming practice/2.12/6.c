e393482275009634951ab9745c9f64315245df34
Codedragon233 <1627173301@qq.com>
2018-08-18 18:25:41 +0800

    add

---
+++ C Primer Plus/Programming practice/2.12/6.c
@@ -0,0 +1,15 @@
+/***********************************************************************/
+/*                              practice 6                             */
+/***********************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	int toes = 10;
+	int double_toes = toes * 2;
+	int square_toes = toes * toes;
+	printf("the value of toes is%d \n the value of double toes is %d \n the value of square toes is %d", toes, double_toes, square_toes);
+
+	return 0;
+}
---
+++ C Primer Plus/Programming practice/2.12/7.c
@@ -0,0 +1,24 @@
+/***********************************************************************/
+/*                              practice 7                             */
+/***********************************************************************/
+
+#include <stdio.h>
+
+void smile();
+
+int main()
+{
+	smile(); smile(); smile();
+	printf("\n");
+	smile(); smile();
+	printf("\n");
+	smile();
+	printf("\n");
+
+	return 0;
+}
+
+void smile()
+{
+	printf("Smile!");
+}
