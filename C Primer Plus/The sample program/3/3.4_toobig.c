7aa3d634634fe1a4b805cba774f1a01b3d8a5219
Codedragon233 <1627173301@qq.com>
2018-08-19 11:08:32 +0800

    Update

---
+++ C Primer Plus/The sample program/3/3.4_toobig.c
@@ -0,0 +1,16 @@
+/* toobig.c */
+#include <stdio.h>
+int main (void)
+{
+  int i = 2147483647;
+  unsigned int j = 4294967295;
+
+  printf("%d %d %d\n", i, i+1, i+2);
+  printf("%u %u %u\n", j, j+1, j+2);
+
+  return 0;
+}
+
+// result:
+//       2147483647 -2147483648 -2147483647
+//       4294967295  0           1
