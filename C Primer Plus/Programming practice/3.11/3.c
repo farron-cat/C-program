4729d59492da5edfd4277667271e6e0e5adfa0f4
Codedragon233 <1627173301@qq.com>
2018-08-24 15:37:19 +0800

    Update

---
+++ C Primer Plus/Programming practice/3.11/3.c
@@ -0,0 +1,11 @@
+/***********************************************************************/
+/*                              practice 3                             */
+/***********************************************************************/
+#include <stdio.h>
+int main (void)
+{
+  printf("\aStartled by the sudden sound, Sally shouted,\n", );
+  printf("\"By the Great Pumpkin, what was that!\"\n", );
+
+  return 0;
+}
